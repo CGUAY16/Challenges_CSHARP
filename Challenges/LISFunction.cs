63c5ddd3d6ec20a20c62539cb4884b59a5b363d8
Chris Guay <rmgchris@hotmail.com>
2020-08-21 16:59:57 -0400

    Add project files.

---
+++ Challenges/LISFunction.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    class LISFunction
+    {
+        public int LongestIncreasingSequence(int[] intArray)
+        {
+            int finalLargestLISLength = 0;
+            int counter = 0;
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (i == 0)
+                {
+                    x = intArray[i];
+                }
+                else
+                {
+                    x = intArray[i];
+                    y = intArray[i - 1];
+
+                    /* debug lines
+                    Console.WriteLine(finalLargestLISLength);
+                    Console.WriteLine(counter);
+                    Console.WriteLine();
+                    */
+
+                    if (x >= y)
+                    {
+                        counter++; // counter increases if LIS is not complete
+
+                        if (i == (intArray.Length - 1) && finalLargestLISLength < counter) // when the LIS is found at last element or array given is sorted
+                        {
+                            finalLargestLISLength = counter;
+                        }
+                    }
+                    else
+                    {
+                        if (finalLargestLISLength < counter) // make counter the new final result for LIS final 
+                        {
+                            finalLargestLISLength = counter;
+                            counter = 0;
+                        }
+                        else // reset counter back to 0
+                        {
+                            counter = 0;
+                        }
+                    }
+                }
+            }
+            return finalLargestLISLength;
+        }
+    }
+}
---
+++ Challenges/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // references
+            LISFunction lisFunction = new LISFunction();
+
+            //
+            // This Tests A function that returns the Longest Increasing Sequence of Positive Ints
+            // Within an Array.
+            //
+            int[] arrayOfPosInts1 = new int[10] { 2, 4, 6, 8, 9, 3, 1, 5, 3, 4 };
+            int[] arrayOfPosInts2 = new int[5] {4, 3, 5, 1, 6};
+
+            Console.WriteLine(arrayOfPosInts1.Length);
+            Console.WriteLine();
+
+            int resultLIS = lisFunction.LongestIncreasingSequence(arrayOfPosInts1);
+            Console.WriteLine(resultLIS);
+            Console.WriteLine();
+
+            resultLIS = lisFunction.LongestIncreasingSequence(arrayOfPosInts2);
+            Console.WriteLine(resultLIS);
+            Console.WriteLine();
+        }
+    }
+
+}
+
